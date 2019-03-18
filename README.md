a0dccfd865669baa7e1b6ffd9ff863dc510024e2
Neal Bohling <neal.bohling@gmail.com>
2019-03-17 21:53:55 -0700

    Rollup recent changes

--- README.md
+++ README.md
@@ -1,22 +1,31 @@
 # toast-timer
+
 ### Timer for Toastmasters
+
 A basic timer to indicate to a speaker how long they have been speaking through the use of color. The intervals are displayed at the lower left corner of the screen and are adjustable.
 
-![screenshot](https://github.com/nbohling/toast-timer/blob/master/timer1.jpeg)
+![screenshot](https://raw.githubusercontent.com/nbohling/toast-timer/master/timer1.jpeg)
 
 Once the timer is started, the background of the page will change through 5 colors:
-* **Gray** is the starting color and means the speaker has plenty of time.
-* **Green** indicates that the speaker has passed the first time checkpoint and is free to continue speaking.
-* **Yellow** indicates that the speaker has passed the second checkpoint and should start wrapping up.
-* **Red** indicates that the speaker has passed the second checkpoint and should stop as soon as possible.
-* **Black** indicates that the speaker has overrun their time and is disqualified from voting.
+
+- **Gray** is the starting color and means the speaker has plenty of time.
+- **Green** indicates that the speaker has passed the first time checkpoint and is free to continue speaking.
+- **Yellow** indicates that the speaker has passed the second checkpoint and should start wrapping up.
+- **Red** indicates that the speaker has passed the second checkpoint and should stop as soon as possible.
+- **Black** indicates that the speaker has overrun their time and is disqualified from voting.
 
 ### Controls:
-* **SPACE** : Start and stop timer. Continues from the last stop.
-* **ENTER** : Start and stop timer. Resets timer to zero when starting.
-* **r** : Reset the timer to zero.
-* **+ and -** : Add or remove 15 seconds to the notification times.
-* **[ and ]** : Add or remove 15 seconds to the interval between the different indications.
-* **1** : Quick reference 1/1.5/2m timer.
-* **2** : Quick reference 3/4/5m timer.
-* **?** : Show/hide the help information.
+
+- **SPACE** : Start and stop timer. Continues from the last stop.
+- **ENTER** : Start and stop timer. Resets timer to zero when starting.
+- **r** : Reset the timer to zero.
+- **c** : Show/hide the mouse controls.
+- **+ and -** : Add or remove 15 seconds to the notification times.
+- **[ and ]** : Add or remove 15 seconds to the interval between the different indications.
+- **Presets:**
+  - **1** : Quick reference 1/1:30/2m timer.
+  - **2** : Quick reference 2/2:30/3m timer.
+  - **3** : Quick reference 3/4/5m timer.
+  - **4** : Quick reference 4/5/6m timer.
+  - **5** : Quick reference 5/6/7m timer.
+- **?** : Show/hide the help information.
--- js/timer.js
+++ js/timer.js
@@ -3,57 +3,113 @@ var myTimer = false;
 var startedTime = 0;
 var elapsedTime = 0;
 var currentInterval = 0;
-var time1 = 60;
-var time2 = 30;
+var first_time = 60;
+var interval_time = 30;
 var transitionTime = 500;
 
 // Color definitions. Change color scheme by adjusting these.
-var gray = "#334141"
+var gray = "#334141";
 var green = "#66B464";
 var yellow = "#FABC3C";
 var red = "#FF5B2D";
-var black = "#000000"
-var white = "#FFF"
+var black = "#000000";
+var white = "#FFF";
+
 // Setup when document loaded.
-$(document).ready(function() {
+$(function() {
   reset();
-  $(".gray").animate({color: gray}, transitionTime);
-  $(".green").animate({color: green}, transitionTime);
-  $(".yellow").animate({color: yellow}, transitionTime);
-  $(".red").animate({color: red}, transitionTime);
-  $(".black").animate({color: black}, transitionTime);
-  blink_red("#controls");
+  $(".gray").animate({ color: gray }, transitionTime);
+  $(".green").animate({ color: green }, transitionTime);
+  $(".yellow").animate({ color: yellow }, transitionTime);
+  $(".red").animate({ color: red }, transitionTime);
+  $(".black").animate({ color: black }, transitionTime);
+  blink_green("#controls");
+
+  // Set up the control panel
+  // Slider for first notification time
+  $(".int_slider_start").slider({
+    min: 30,
+    max: 10 * 60,
+    value: 60,
+    step: 15,
+    slide: (event, ui) => {
+      first_time = ui.value;
+      updateIntervals();
+    }
+  });
+
+  // Slider for interval between notifications
+  $(".int_slider_between").slider({
+    min: 15,
+    max: 5 * 60,
+    step: 15,
+    value: 30,
+    slide: (event, ui) => {
+      interval_time = ui.value;
+      updateIntervals();
+    }
+  });
+
+  // Start/Stop button
+  $("button.start_stop")
+    .button()
+    .click(() => {
+      timerToggle(false);
+    });
+
+  // Reset button
+  $("button.reset")
+    .button()
+    .click(() => {
+      reset();
+    });
+
+  $("button.preset_speaker")
+    .button()
+    .click(() => {
+      first_time = 4 * 60;
+      interval_time = 60;
+      updateIntervals();
+    });
+
+  $("button.preset_table_topics")
+    .button()
+    .click(() => {
+      first_time = 1 * 60;
+      interval_time = 30;
+      updateIntervals();
+    });
 
   // Setup controller for key strokes
   $(document).keypress(function(event) {
-    console.log("Pressed: "+event.which);
-    switch(event.which) {
+    console.log("Pressed: " + event.which);
+    switch (event.which) {
       case 13: // Enter
-        timerToggle(true);
+        if (event.target.nodeName != "BUTTON") timerToggle(true);
         break;
       case 32: // Space
-        timerToggle(false);
+        if (event.target.nodeName != "BUTTON") timerToggle(false);
         break;
       case 61: // =
       case 43: // +
-        if(time1<15) time1 = time1 + 5;
-        else time1 = time1 + 15;
+        if (first_time < 15) first_time = first_time + 5;
+        else first_time = first_time + 15;
         break;
       case 45: // -
-        if(time1 <= 15) time1 = time1 - 5;
-        else time1 = time1 - 15;
+        if (first_time <= 15) first_time = first_time - 5;
+        else first_time = first_time - 15;
 
-        if(time1<5) time1 = 5;
+        if (first_time < 5) first_time = 5;
         break;
       case 93: // [
-        if(time2<15) time2 = time2 + 5;
-        else time2 = time2 + 15;
+        if (interval_time < 15) interval_time = interval_time + 5;
+        else interval_time = interval_time + 15;
         break;
       case 91: // ]
-        if(time2 <= 15) time2 = time2 - 5;
-        else time2 = time2 - 15;
+        if (interval_time <= 15) interval_time = interval_time - 5;
+        else interval_time = interval_time - 15;
 
-        if(time2 < 5) time2 = 5;
+        if (interval_time < 5) interval_time = 5;
         break;
       case 63: // ?
         $("#help_page").fadeToggle(transitionTime);
@@ -61,138 +117,189 @@ $(document).ready(function() {
       case 114: // r - reset
         reset();
         break;
-      case 48: // 0 - shorted preset
-        time1 = 5;
-        time2 = 5;
+      case 48: // 0 - shortest preset
+        first_time = 5;
+        interval_time = 5;
         break;
       case 49: // 1
-        time1 = 60;
-        time2 = 30;
+        first_time = 60;
+        interval_time = 30;
         break;
       case 50: // 2
-        time1 = 3*60;
-        time2 = 60;
+        first_time = 2 * 60;
+        interval_time = 30;
+        break;
+      case 51: // 3
+        first_time = 3 * 60;
+        interval_time = 60;
+        break;
+      case 52: // 4
+        first_time = 4 * 60;
+        interval_time = 60;
+        break;
+      case 53: // 5
+        first_time = 5 * 60;
+        interval_time = 60;
+        break;
+      case 99: // c
+        $("#control_panel").fadeToggle();
         break;
       case 120: // x
-        $("#timer").fadeToggle();
+        //$('#timer').fadeToggle();
+        $("#controls").fadeToggle();
+        $("#footer").fadeToggle();
         break;
     }
     updateIntervals();
-  })
-});
+  });
 
-// Toggles whether timer is running or not
-// Reset indicates whether to start from zero or continue from last time
-function timerToggle(reset) {
-  if( myTimer ) stopTimer();
-  else startTimer(reset);
-}
+  // Toggles whether timer is running or not
+  // Reset indicates whether to start from zero or continue from last time
+  function timerToggle(reset) {
+    if (myTimer) stopTimer();
+    else startTimer(reset);
+  }
 
-// Starts the timer event loop
-function startTimer(reset) {
-  //$(".indicator").animate({backgroundColor: "green"}, 1transitionTime);
-  if(reset) {
-    $(".indicator").animate({backgroundColor: gray}, transitionTime);
+  // Starts the timer event loop
+  function startTimer(reset) {
+    //$(".indicator").animate({backgroundColor: "green"}, 1transitionTime);
+    if (reset) {
+      $(".indicator").animate({ backgroundColor: gray }, transitionTime);
+      startedTime = Date.now();
+      currentInterval = 0;
+    } else {
+      startedTime = Date.now() - elapsedTime;
+    }
+    myTimer = setInterval(updateTimer, 25);
+  }
+
+  // Stops the timer event loop
+  function stopTimer() {
+    //elapsedTime = Date.now() - startedTime;
+    window.clearInterval(myTimer);
+    myTimer = false;
+    console.log("Stopped");
+  }
+
+  // Updates the timer display including color and time value
+  function updateTimer() {
+    if (myTimer) elapsedTime = Date.now() - startedTime;
+
+    ms = elapsedTime;
+    sec = ms / 1000;
+    m = Math.floor(ms / (1000 * 60));
+    ms = ms - m * 1000 * 60;
+    s = Math.floor(ms / 1000);
+    ms = ms - s * 1000;
+    ds = Math.floor(ms / 100);
+
+    timeString = "" + m + ":" + ensure2Digits(s) + "." + ds;
+    $("#timer").text(timeString);
+
+    // Thresholds (in s)
+    const threshold_green = first_time;
+    const threshold_yellow = threshold_green + interval_time;
+    const threshold_red = threshold_yellow + interval_time;
+    const threshold_black = threshold_red + Math.min(interval_time, 30);
+
+    // See if we jumped over a barrier
+    if (currentInterval != 0 && sec >= 0 && sec < threshold_green) {
+      $(".indicator").animate({ backgroundColor: gray }, transitionTime);
+      currentInterval = 0;
+    } else if (
+      currentInterval != 1 &&
+      sec >= threshold_green &&
+      sec < threshold_yellow
+    ) {
+      $(".indicator").animate({ backgroundColor: green }, transitionTime);
+      currentInterval = 1;
+    } else if (
+      currentInterval != 2 &&
+      sec >= threshold_yellow &&
+      sec < threshold_red
+    ) {
+      $(".indicator").animate({ backgroundColor: yellow }, transitionTime);
+      currentInterval = 2;
+    } else if (
+      currentInterval != 3 &&
+      sec >= threshold_red &&
+      sec < threshold_black
+    ) {
+      $(".indicator").animate({ backgroundColor: red }, transitionTime);
+      currentInterval = 3;
+    } else if (currentInterval != 4 && sec >= threshold_black) {
+      $(".indicator").animate({ backgroundColor: black }, transitionTime);
+      currentInterval = 4;
+    }
+  }
+
+  // Updates the interval numbers
+  function updateIntervals() {
+    if (first_time >= 60) {
+      $(".int_stop1").text(toMinuteAndSeconds(first_time));
+      $(".int_stop2").text(toMinuteAndSeconds(first_time + interval_time));
+      $(".int_stop3").text(toMinuteAndSeconds(first_time + interval_time * 2));
+      $(".int_stop4").text(
+        toMinuteAndSeconds(
+          first_time + interval_time * 2 + Math.min(interval_time, 30)
+        )
+      );
+      $(".int_between").text(toMinuteAndSeconds(interval_time));
+      $(".int_units").text("min");
+    } else {
+      $(".int_stop1").text(first_time);
+      $(".int_stop2").text(first_time + interval_time);
+      $(".int_stop3").text(first_time + interval_time * 2);
+      $(".int_stop4").text(
+        first_time + interval_time * 2 + Math.min(interval_time, 30)
+      );
+      $(".int_units").text("sec");
+      $(".int_between").text(interval_time);
+    }
+    updateTimer();
+    // Try to update the sliders
+    try {
+      $(".int_slider_start").slider("value", first_time);
+      $(".int_slider_between").slider("value", interval_time);
+    } catch (error) {
+      console.log("Slider isn't ready.");
+    }
+  }
+
+  // Convert seconds to a string that represents minutes and seconds
+  function toMinuteAndSeconds(seconds) {
+    if (seconds < 60) return "0:" + seconds;
+    else {
+      minutes = Math.floor(seconds / 60);
+      seconds2 = seconds - minutes * 60;
+      return minutes + ":" + ensure2Digits(seconds2);
+    }
+  }
+
+  // Resets everything and stops any active timers.
+  function reset() {
+    $(".indicator").animate({ backgroundColor: gray }, transitionTime);
+    currentInterval = 0;
+    if (myTimer) stopTimer();
     startedTime = Date.now();
-    currentInterval = 0;
-  } else {
-    startedTime = Date.now() - elapsedTime;
+    elapsedTime = 0;
+    updateTimer();
+    updateIntervals();
   }
-  myTimer = setInterval(updateTimer, 25);
-}
 
-// Stops the timer event loop
-function stopTimer() {
-  //elapsedTime = Date.now() - startedTime;
-  window.clearInterval(myTimer);
-  myTimer = false;
-  console.log("Stopped");
-}
-
-// Updates the timer display
-function updateTimer() {
-  if(myTimer) elapsedTime = Date.now() - startedTime
-
-  ms = elapsedTime;
-  sec = ms/1000;
-  m = Math.floor(ms / (1000 * 60));
-  ms = ms - (m*1000*60);
-  s = Math.floor(ms / 1000);
-  ms = ms - (s*1000);
-  ds = Math.floor(ms/100);
-
-  timeString = ""+m + ":" + ensure2Digits(s) + "." + ds;
-  $("#timer").text(timeString);
-
-  // See if we jumped over a barrier
-  if( currentInterval != 0 && sec >= 0 && sec < time1 ) {
-    $(".indicator").animate({backgroundColor: gray}, transitionTime);
-    currentInterval = 0;
-  } else if( currentInterval != 1 && sec >= time1 && sec < time1 + time2 ) {
-    $(".indicator").animate({backgroundColor: green}, transitionTime);
-    currentInterval = 1;
-  } else if( currentInterval != 2 && sec >= time1 + time2 && sec < time1 + (time2*2) ) {
-    $(".indicator").animate({backgroundColor: yellow}, transitionTime);
-    currentInterval = 2;
-  } else if( currentInterval != 3 && sec >= time1 + (time2*2) && sec < time1 + (time2*3) ) {
-    $(".indicator").animate({backgroundColor: red}, transitionTime);
-    currentInterval = 3;
-  } else if( currentInterval != 4 && sec >= time1 + (time2*3) ) {
-    $(".indicator").animate({backgroundColor: black}, transitionTime);
-    currentInterval = 4;
+  // Helper function to ensure 2 digits on a number.
+  function ensure2Digits(number) {
+    if (number < 10) return "0" + number;
+    else return number;
   }
-}
 
-// Updates the interval displays
-function updateIntervals() {
-  if(time1 >= 60) {
-    $("#stop1").text(toMinuteAndSeconds(time1));
-    $("#stop2").text(toMinuteAndSeconds(time1+time2));
-    $("#stop3").text(toMinuteAndSeconds(time1+(time2*2)));
-    $("#stop4").text(toMinuteAndSeconds(time1+(time2*3)));
-    $("#units").text("min");
-  } else {
-    $("#stop1").text(time1);
-    $("#stop2").text(time1+time2);
-    $("#stop3").text(time1+(time2*2));
-    $("#stop4").text(time1+(time2*3));
-    $("#units").text("sec");
+  // Alternate between green and red
+  function blink_green(selector) {
+    blink = 6;
+    intID = window.setInterval(function() {
+      if (blink % 2 == 0) $(selector).animate({ color: green }, 300);
+      else $(selector).animate({ color: white }, 300);
+      if (--blink == 0) window.clearInterval(intID);
+    }, 500);
   }
-  updateTimer();
-}
-
-function toMinuteAndSeconds(seconds) {
-  if(seconds < 60) return ""+seconds
-  else {
-    minutes = Math.floor(seconds/60);
-    seconds2 = seconds - (minutes*60);
-    return minutes+":"+ensure2Digits(seconds2);
-  }
-}
-
-// Resets everything and stops any active timers.
-function reset() {
-  $(".indicator").animate({backgroundColor: gray}, transitionTime);
-  currentInterval = 0;
-  if( myTimer ) stopTimer();
-  startedTime = Date.now();
-  elapsedTime = 0;
-  updateTimer();
-  updateIntervals();
-}
-
-// Helper function to ensure 2 digits on a number.
-function ensure2Digits(number) {
-  if( number < 10 ) return "0"+number;
-  else return number;
-}
-
-function blink_red(selector) {
-  var blink=6;
-  var intID = window.setInterval(function() {
-    if(blink%2==0) $(selector).animate({color: green}, 300);
-    else $(selector).animate({color: white}, 300);
-    if(--blink==0) window.clearInterval(intID);
-  },
-  500);
-}
+});
